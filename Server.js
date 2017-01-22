efa6a1c2f9659f43aa03c7191fa9ff5b81ab5402
nihar07 <nihargoswami@gmail.com>
2017-01-22 12:54:17 -0500

    t

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 
 var azure = require('azure');
 
-var http = require('http')
+var http = require('http');
  var port = process.env.PORT || 1337;
  http.createServer(function(req, res) {
    res.writeHead(200, { 'Content-Type': 'text/plain' });
