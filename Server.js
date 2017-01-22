e5390b3b062e7788ca9e4f3451974124129fb0f4
nihar07 <nihargoswami@gmail.com>
2017-01-22 18:50:42 -0500

    add receive

--- Server.js
+++ Server.js
@@ -5,3 +5,21 @@ var http = require('http')
    res.writeHead(200, { 'Content-Type': 'text/plain' });
    res.end('Hello World\n');
  }).listen(port);
+ var azure = require('azure');
+console.log("loaded Azure Library");
+var connectionString = "Endpoint=sb://sensetest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Dcg4pmHWWyDbxRUIFC2VEbRSOsY6kXqH90ysHwP1xz0=";
+console.log("Connection string");
+var serviceBusService = azure.createServiceBusService(connectionString);
+console.log("Connected to Azure");
+var message = {
+    body: 'Test message',
+    customProperties: {
+        testproperty: 'TestValue'
+    }};
+console.log("message creates");
+serviceBusService.receiveQueueMessage('myqueue', function(error, receivedMessage){
+    if(!error){
+        console.log("message Received");// Message received and deleted
+console.log(receivedMessage);
+    }
+});
