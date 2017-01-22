324b58d6a3726c1ad638ab4d5925b6f7d91e6938
nihar07 <nihargoswami@gmail.com>
2017-01-22 12:48:52 -0500

    test2

--- Server.js
+++ Server.js
@@ -1,4 +1,17 @@
- var http = require('http')
+
+var azure = require('azure');
+var serviceBusService = azure.createServiceBusService();
+var message = {
+    body: 'Test message',
+    customProperties: {
+        testproperty: 'TestValue'
+    }};
+serviceBusService.sendQueueMessage('myqueue', message, function(error){
+    if(!error){
+        // message sent
+    }
+});
+var http = require('http')
  var port = process.env.PORT || 1337;
  http.createServer(function(req, res) {
    res.writeHead(200, { 'Content-Type': 'text/plain' });
