6292668151d594ca1dc7a7590e5f9b9ffe2a6541
nihar07 <nihargoswami@gmail.com>
2017-01-22 19:03:56 -0500

    yes

--- Server.js
+++ Server.js
@@ -1,10 +1,4 @@
 
-var http = require('http')
- var port = process.env.PORT || 1337;
- http.createServer(function(req, res) {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end('Hello World\n');
- }).listen(port);
  var azure = require('azure');
 console.log("loaded Azure Library");
 var connectionString = "Endpoint=sb://sensetest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Dcg4pmHWWyDbxRUIFC2VEbRSOsY6kXqH90ysHwP1xz0=";
@@ -16,10 +10,20 @@ var message = {
     customProperties: {
         testproperty: 'TestValue'
     }};
+    var temp = "yes"
 console.log("message creates");
 serviceBusService.receiveQueueMessage('myqueue', function(error, receivedMessage){
     if(!error){
         console.log("message Received");// Message received and deleted
 console.log(receivedMessage);
+temp = receivedMessage.body;
     }
 });
+
+var http = require('http')
+ var port = process.env.PORT || 1337;
+ http.createServer(function(req, res) {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   res.
+   res.end(temp);
+ }).listen(port);
